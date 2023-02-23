dc47002e730342c7e697ada5fe1ed25d11b92927
TLRKiliann <philogenie@protonmail.com>
2023-02-23 13:46:21 +0100

    some changes

--- my-app/pages/news/index.tsx
+++ my-app/pages/news/index.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from 'next'
+import Link from 'next/link'
 
 type ArticlesProps = {
   articles: {
@@ -16,13 +17,13 @@ const NewArticles:React.FC = ({ articles }: ArticlesProps) => {
       <h1>List of News Articles</h1>
       {articles.map((article: any)=> (
         <div key={article.id}>
+          <Link href={`/news/${article.category}`}>{article.title}</Link>
           <p>{article.category}</p>
         </div>
       ))}
     </div>
   )
 }
-
 export default NewArticles
 
 export const getServerSideProps: GetServerSideProps = async () => {
