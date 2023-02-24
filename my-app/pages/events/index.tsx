5d6aded9616ed3f4244d5271cfcb9be56689c0d6
TLRKiliann <philogenie@protonmail.com>
2023-02-24 23:31:53 +0100

    new route for client side with SSR

---
+++ my-app/pages/events/index.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+
+type EnventListProps = {
+  eventList: {
+    id: number
+    article: string
+    location: string
+    category: string
+  }
+}
+
+const EventList = ({ eventList }: EnventListProps) => {
+  const router = useRouter() as any
+  const [events, setEvents] = useState<any>(eventList)
+  const [isSwitched, setIsSwitched] = useState<boolean>(true)
+
+  const fetchByCategory = async (category: string) => {
+    const response = await fetch(`http://localhost:4000/events?category=${category}`)
+    const data = await response.json()
+    setEvents(data)
+    setIsSwitched(false)
+    router.push(`/events?category=${category}`, undefined, { shallow: true })
+  }
+
+  const handleBackHome = () => {
+    router.push('/')
+  }
+  return (
+    <div>
+      <Head>
+        <title>Events</title>
+        <meta name="description" content="Events page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {events.map((event: any) => (
+        <div key={event.id}> 
+          <h2>{event.article}</h2>
+          <h2>{event.location}</h2>
+          <h2>{event.category}</h2>
+          {isSwitched ? (
+            <button
+              type="button"
+              onClick={() => fetchByCategory(event.category)}>
+              Choose this category
+            </button>
+            ) : (
+            <button
+              type='button'
+              onClick={handleBackHome}
+            >
+              Return
+            </button>
+            )
+          }
+          <hr />
+        </div>
+      ))}
+    </div>
+  )
+}
+export default EventList
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch('http://localhost:4000/events')
+  const data = await response.json()
+  return {
+    props: {
+      eventList: data,
+    }
+  }
+}
