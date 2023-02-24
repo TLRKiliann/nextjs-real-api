ea06d0e3b6afb4bd5d69eb519ab9020fc87d3cec
TLRKiliann <philogenie@protonmail.com>
2023-02-24 23:32:24 +0100

    styles conf & import

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app'
+import "@fontsource/noto-sans-newa"
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
